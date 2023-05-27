d357b1165fd77e1e76fe70af58dbce56ee5c1ae8
Phạm Đình Lâm <62865687+phamdinhlam105@users.noreply.github.com>
2023-05-27 07:41:21 +0700

    Delete Bai03 directory

--- Bai03/Bai02.js
+++
@@ -1,9 +0,0 @@
-function timGiaTriLonNhat(so) {
-    let max = so[0];
-    for(i=1;i<so.length;i++)
-        if(so[i]>max)
-            max=so[i];
-    return max;
-   }
-   // Ví dụ sử dụng
-   console.log(timGiaTriLonNhat([5, 8, 3, 2, 9])); 
--- Bai03/bai01.js
+++
@@ -1,8 +0,0 @@
-function KiemTraChanLe(so){
-    if(so%2==0)
-        return 'Chan'
-    else
-        return 'Le'
-}
-console.log(KiemTraChanLe(7));
-console.log(KiemTraChanLe(8));
